c87f6d20a57f76a87700eb2c325961d7c913251f
Chris Lundquist <chris.lundquist@bluebox.net>
2012-10-24 18:27:02 -0700

    [default] simplify API to just be a hash of services pointing to the actions

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,10 +17,8 @@
 # limitations under the License.
 #
 
-node.services.each do |service_name_and_actions|
-  service_name_and_actions.each_pair do |service_name, actions|
-    service service_name do
-      action actions
-    end
+node.services.each_pair do |service_name, actions|
+  service service_name do
+    action actions
   end
 end
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -7,7 +7,7 @@ describe 'services::default' do
 
   context "when the services attribute is an empty array" do
     before(:each) do
-      chef.node.set['services'] = Array.new
+      chef.node.set['services'] = Hash.new
       chef.converge 'services::default'
     end
 
@@ -18,7 +18,7 @@ describe 'services::default' do
 
   context "when the services attribute is populated with a stop value" do
     before(:each) do
-      chef.node.set['services'] = [ { :cups => [:stop] } ]
+      chef.node.set['services'] = { :cups => [:stop] }
       chef.converge 'services::default'
     end
 
@@ -29,7 +29,7 @@ describe 'services::default' do
 
   context "when the services attribute is populated with a start value" do
     before(:each) do
-      chef.node.set['services'] = [ { :cups => [:start] } ]
+      chef.node.set['services'] = { :cups => [:start] }
       chef.converge 'services::default'
     end
 
@@ -40,7 +40,7 @@ describe 'services::default' do
 
   context "when life is messy and we are stopping and disabling a service" do
     before(:each) do
-      chef.node.set['services'] = [ { :cups => [:stop,:disable] } ]
+      chef.node.set['services'] = { :cups => [:stop,:disable] }
       chef.converge 'services::default'
     end
 
@@ -48,6 +48,30 @@ describe 'services::default' do
       chef.should stop_service 'cups'
       # https://github.com/acrmp/chefspec/pull/67
       # chef.should disable_service 'cups'
+    end
+  end
+
+  context "when life is messy and we are stopping many services" do
+    before(:each) do
+      chef.node.set['services'] = { :cups => [:stop], :bluetooth => [:stop] }
+      chef.converge 'services::default'
+    end
+
+    it 'should stop the service' do
+      chef.should stop_service 'cups'
+      chef.should stop_service 'bluetooth'
+    end
+  end
+
+  context "when the values are strings" do
+    before(:each) do
+      chef.node.set['services'] = { :cups => ['stop'], :bluetooth => ["stop"] }
+      chef.converge 'services::default'
+    end
+
+    it 'should stop the service' do
+      chef.should stop_service 'cups'
+      chef.should stop_service 'bluetooth'
     end
   end
 end
